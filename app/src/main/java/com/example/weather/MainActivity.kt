707c3cf7be01693d22f6248946d3b7fd01619e7b
Prashant Dahal <prashantdah41@gmail.com>
2021-08-02 16:15:57 -0400

    Check if location is turned on.

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.weather
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!isLocationOn()) {
+            Toast.makeText(this, "Turn on your location", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+
+        }
+
     }
+
+    private fun isLocationOn(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
 }
