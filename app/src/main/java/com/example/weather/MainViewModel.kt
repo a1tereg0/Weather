ec693d1107e659367bcc3be86fe69f890bc11194
prashant dahal <prashantdah41@gmail.com>
2021-08-14 16:54:42 -0400

    Implement view model

--- app/src/main/java/com/example/weather/MainViewModel.kt
+++ app/src/main/java/com/example/weather/MainViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.weather
 
-class MainViewModel {
+import androidx.lifecycle.*
+import com.example.weather.network.models.WeatherData
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class MainViewModel(
+    private val repository: WeatherRepository
+): ViewModel() {
+    val liveWeatherData: LiveData<WeatherData> get() = repository.liveWeatherData
+
+//    init {
+//        viewModelScope.launch {
+//            repository.loadWeatherData()
+//        }
+//    }
+
+fun afterSuccessfullyFetchedLocationData(coordinate: Coordinate) {
+    viewModelScope.launch {
+        repository.loadWeatherData(coordinate)
+    }
+}
+
+}
+
+class MainViewModelFactory(private val repository: WeatherRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+       if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+           return MainViewModel(repository) as T
+       }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
