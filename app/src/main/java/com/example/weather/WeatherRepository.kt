97755838dcbd0a6e381313dbf56075a0100feed5
prashant dahal <prashantdah41@gmail.com>
2021-08-14 16:55:16 -0400

    Implement repository class

--- app/src/main/java/com/example/weather/WeatherRepository.kt
+++ app/src/main/java/com/example/weather/WeatherRepository.kt
@@ -1,4 +1,23 @@
 package com.example.weather
 
-class WeatherRepository {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.weather.network.ApiService
+import com.example.weather.network.models.WeatherData
+
+class WeatherRepository(private val apiService: ApiService) {
+    private val _liveWeatherData = MutableLiveData<WeatherData>()
+    val liveWeatherData: LiveData<WeatherData> get() = _liveWeatherData
+
+    suspend fun loadWeatherData(coordinate: Coordinate) {
+        val result = try {
+            apiService.getWeatherData(coordinate.latitude, coordinate.longitude, appid = BundleKeys.API_KEY)
+        } catch (e: Exception){
+            null
+        }
+        result?.let {
+            _liveWeatherData.value = it
+        }
+    }
 }
